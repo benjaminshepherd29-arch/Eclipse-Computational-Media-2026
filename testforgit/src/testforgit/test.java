15067a9d82907dc90b38407c9d679cec8f5665d7
Ben <Ben@192.168.1.212>
2026-04-28 21:57:01 -0400

    yo

--- testforgit/src/testforgit/test.java
+++ testforgit/src/testforgit/test.java
@@ -3,7 +3,7 @@ package testforgit;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("a_+b");
+		System.out.println("a_+b i like 67 great 67 ohj 67 nuice mid 67");
 	}
 	
 }
